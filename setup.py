e8761ecc5e7f294b2d5ed195af2e57ad0e9e1db6
Oliver Barnum <oliverbarnum32@gmail.com>
2020-05-25 23:27:15 -0700

    Remove lark without parser

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description = "Creates a ui to view Stellaris event information",
     url = "",
     packages=find_packages(),
-    install_requires=["click", "ensure", "luaparser", "pyyaml", "lark", "lark-parser"],
+    install_requires=["click", "ensure", "luaparser", "pyyaml", "lark-parser"],
     entry_points = {
         "console_scripts": ["stellarisfiles=stellarisfiles.cli:cli"]
     },
