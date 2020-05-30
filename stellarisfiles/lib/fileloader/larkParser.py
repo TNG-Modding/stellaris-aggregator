000234c219a80b190e5a491009556ef01c1d5005
Oliver Barnum <oliverbarnum32@gmail.com>
2020-05-29 23:31:24 -0700

    Get appoximation of json for events

--- stellarisfiles/lib/fileloader/larkParser.py
+++ stellarisfiles/lib/fileloader/larkParser.py
@@ -4,13 +4,37 @@ from pathlib import Path
 from pprint import pprint
 
 class EventTransformer(Transformer):
-    def list(self, items):
-        return list(items)
-    def pair(self, key_value):
-        k, v = key_value
-        return k, v
-    def dict(self, items):
-        return dict(items)
+    def define(self, args):
+        definition = {}
+        items = []
+
+        i = 2
+        while i < len(args):
+            items.append(args[i])
+            i = i + 1            
+        
+        if args[1] != "=":
+            definition["%s%s" % (args[0], args[1])] = items
+        else:
+            definition[args[0]] = items
+        return definition
+    def VAR_STR(self, args):
+        return str(args)
+    def MAGIC_VALUES(self, args):
+        return str(args)
+    def DOT_STR(self, args):
+        return str(args)
+    def STRING(self, args):
+        return str(args)
+    def set_planet_flag(self, args):
+        return str(args)
+    def start (self, args):
+        definition = []
+        for arg in args:
+            definition.append(arg)
+        return definition
+    def POSS_SYMB(self, args):
+        return args[0]
 
 def ParseEventFile(eventFilepath):
     eventLarkFilepath = os.path.join(Path(os.path.realpath(__file__)).parent, "events.lark")
@@ -18,9 +42,7 @@ def ParseEventFile(eventFilepath):
         eventParser = Lark(eventRules)
         with open (eventFilepath, "r") as events:
             tree = eventParser.parse(events.read())
-            pprint(tree)
-            # print("Made the tree")
-            # transformedTree = EventTransformer().transform(tree)
-            # print("Transformed the tree")
-            # pprint(transformedTree)
+            # pprint(tree)
+            transformedTree = EventTransformer().transform(tree)
+            pprint(transformedTree)
 
