d9b192760fae23485e30235d573d74d3927c8027
Oliver Barnum <oliverbarnum32@gmail.com>
2020-06-29 22:56:45 -0700

    Add dark theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import './dark.css';
 
 import ConversationPanel from "./components/conversationPanel"
 
